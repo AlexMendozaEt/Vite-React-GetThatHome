fa06cc1fa4289ee0eafb1281b37ee9b9ed693b82
Kevin Robles <kevinronu@gmail.com>
2023-07-17 11:57:51 -0500

    chore: add reset css

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Global } from "@emotion/react";
+
 import App from "./App.jsx";
+import { reset, global } from "./styles/global.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Global styles={reset} />
+    <Global styles={global} />
     <App />
   </React.StrictMode>
 );
