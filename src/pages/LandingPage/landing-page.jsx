2f8eb5b61cc6a21fdc852d928b9ef35f684a4c3f
Kevin Robles <kevinronu@gmail.com>
2023-07-19 00:51:12 -0500

    feat: add LandingPage

---
+++ src/pages/LandingPage/landing-page.jsx
@@ -0,0 +1,17 @@
+import TeamMembers from "../../components/TeamMembers";
+import SignUpSection from "../../components/SignUpSection";
+import BestPropertiesSection from "../../components/BestPropertiesSection";
+import MeetHomeSection from "../../components/MeetHomeSection";
+
+export default function LandingPage() {
+  return (
+    <>
+      <header>Header</header>
+      <MeetHomeSection />
+      <BestPropertiesSection />
+      <SignUpSection />
+      <TeamMembers />
+      <footer>Footer</footer>
+    </>
+  );
+}
