c3f37d3f77a7740b8dfe77c0f9f3eb05d85b23f9
Alex Mendoza <alex.mendoza.et@gmail.com>
2023-07-19 21:28:30 -0500

    added HomeSeekerPage structure

---
+++ src/pages/HomeSeekerPage/seekerPage.jsx
@@ -0,0 +1,13 @@
+import SavedPropertiesSection from "../../components/SavedPropertiesSection";
+import HeaderSeeker from "../../components/HeaderSeeker";
+import FooterLanding from "../../components/FooterLanding";
+
+export default function SeekerPage() {
+  return (
+    <>
+      <HeaderSeeker />
+      <SavedPropertiesSection />
+      <FooterLanding />
+    </>
+  );
+}
