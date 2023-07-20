856124719ea7ca4cafecf4c87abe274aae81a286
Daniel Martinez <martinezh.dani@gmail.com>
2023-07-19 19:08:12 -0600

    chore: delete old Filters

--- src/components/Filters/Pets/index.js
+++
@@ -1,3 +0,0 @@
-import { Container, ContainerCard, Title, PriceContainer, ButtonContainer, ContainerIcon, ContainerInput, Input, InputCheckbox} from "./style";
-
-export { Container, ContainerCard, Title, PriceContainer, ButtonContainer, ContainerIcon, ContainerInput, Input, InputCheckbox};
