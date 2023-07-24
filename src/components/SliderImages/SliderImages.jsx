3ebd94d1a03cd52be8fcd895085d684856bebef7
Alex Mendoza <alex.mendoza.et@gmail.com>
2023-07-24 13:23:18 -0500

    Create Slider Component

---
+++ src/components/SliderImages/SliderImages.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import PropTypes from "prop-types";
+
+import {
+  slideStyles,
+  rightArrowStyles,
+  leftArrowStyles,
+  sliderStyles,
+  dotsContainerStyles,
+  dotStyle,
+} from "./styles";
+
+const ImageSlider = ({ slides }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const goToPrevious = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+  const goToNext = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+  const goToSlide = (slideIndex) => {
+    setCurrentIndex(slideIndex);
+  };
+  const slideStylesWidthBackground = {
+    ...slideStyles,
+    backgroundImage: `url(${slides[currentIndex]})`,
+  };
+
+  return (
+    <div style={sliderStyles}>
+      <div>
+        <div onClick={goToPrevious} style={leftArrowStyles}>
+          ❰
+        </div>
+        <div onClick={goToNext} style={rightArrowStyles}>
+          ❱
+        </div>
+      </div>
+      <div style={slideStylesWidthBackground}></div>
+      <div style={dotsContainerStyles}>
+        {slides.map((slide, slideIndex) => (
+          <div
+            style={dotStyle}
+            key={slideIndex}
+            onClick={() => goToSlide(slideIndex)}
+          >
+            ●
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+ImageSlider.propTypes = {
+  slides: PropTypes.array,
+};
+
+export default ImageSlider;
