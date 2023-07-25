3ec1a9cc1b2d8d24a4c2c5073ded8000cc048101
Kevin Robles <kevinronu@gmail.com>
2023-07-24 22:06:07 -0500

    chore: connect api to best-properties

--- src/components/BestPropertiesSection/best-properties-section.jsx
+++ src/components/BestPropertiesSection/best-properties-section.jsx
@@ -1,4 +1,5 @@
 import PropTypes from "prop-types";
+import { useEffect, useState } from "react";
 
 import {
   PropertiesContainer,
@@ -8,171 +9,36 @@ import {
 } from "./styles";
 import Container from "../../layout/Container/container";
 import PropertyCard from "../PropertyCard";
+import {
+  getMyFavorites,
+  getMyProperties,
+  getProperties,
+} from "../../services/property-service";
 
-const best_mock = [
-  {
-    id: 1,
-    operation_type: 0,
-    address: "1000 Jacob Gateway, Durganport, WV 48044",
-    monthly_rent: 1000,
-    property_type: 0,
-    bedrooms: 4,
-    bathrooms: 2,
-    area: 180,
-    pets_allowed: true,
-    photo_url: [
-      "src/assets/images/background-example.jpg",
-      "src/assets/images/background-example.jpg",
-      "src/assets/images/background-example.jpg",
-      "src/assets/images/background-example.jpg",
-      "src/assets/images/background-example.jpg",
-    ],
-  },
-  {
-    id: 5,
-    operation_type: 1,
-    address: "5000 Jacob Gateway, Durganport, WV 48044",
-    monthly_rent: 2000,
-    property_type: 1,
-    bedrooms: 4,
-    bathrooms: 2,
-    area: 180,
-    pets_allowed: true,
-    photo_url: [
-      "src/assets/images/background-example.jpg",
-      "src/assets/images/background-example.jpg",
-      "src/assets/images/background-example.jpg",
-      "src/assets/images/background-example.jpg",
-      "src/assets/images/background-example.jpg",
-    ],
-  },
-  {
-    id: 9,
-    operation_type: 0,
-    address: "9000 Jacob Gateway, Durganport, WV 48044",
-    monthly_rent: 3000,
-    property_type: 0,
-    bedrooms: 4,
-    bathrooms: 2,
-    area: 180,
-    pets_allowed: false,
-    photo_url: [
-      "src/assets/images/background-example.jpg",
-      "src/assets/images/background-example.jpg",
-      "src/assets/images/background-example.jpg",
-      "src/assets/images/background-example.jpg",
-      "src/assets/images/background-example.jpg",
-    ],
-  },
-];
+export default function BestPropertiesSection() {
+  const [properties, setProperties] = useState([]);
+  const [favorites, setFavorites] = useState([]);
+  const [ownProperties, setOwnProperties] = useState([]);
 
-const favorites_mock = [
-  {
-    id: 1,
-    operation_type: 0,
-    address: "1000 Jacob Gateway, Durganport, WV 48044",
-    monthly_rent: 1000,
-    property_type: 0,
-    bedrooms: 4,
-    bathrooms: 2,
-    area: 180,
-    pets_allowed: true,
-    photo_url: [
-      "src/assets/images/background-example.jpg",
-      "src/assets/images/background-example.jpg",
-      "src/assets/images/background-example.jpg",
-      "src/assets/images/background-example.jpg",
-      "src/assets/images/background-example.jpg",
-    ],
-  },
-  {
-    id: 3,
-    operation_type: 0,
-    address: "3000 Jacob Gateway, Durganport, WV 48044",
-    monthly_rent: 1000,
-    property_type: 0,
-    bedrooms: 3,
-    bathrooms: 2,
-    area: 180,
-    pets_allowed: true,
-    photo_url: [
-      "src/assets/images/background-example.jpg",
-      "src/assets/images/background-example.jpg",
-      "src/assets/images/background-example.jpg",
-      "src/assets/images/background-example.jpg",
-      "src/assets/images/background-example.jpg",
-    ],
-  },
-];
+  useEffect(() => {
+    getProperties()
+      .then((properties) => {
+        setProperties(properties);
+      })
+      .catch((e) => console.log(e));
 
-const contacted_mock = [
-  {
-    id: 6,
-    operation_type: 0,
-    address: "1000 Jacob Gateway, Durganport, WV 48044",
-    monthly_rent: 6000,
-    property_type: 0,
-    bedrooms: 4,
-    bathrooms: 2,
-    area: 180,
-    pets_allowed: true,
-    photo_url: [
-      "src/assets/images/background-example.jpg",
-      "src/assets/images/background-example.jpg",
-      "src/assets/images/background-example.jpg",
-      "src/assets/images/background-example.jpg",
-      "src/assets/images/background-example.jpg",
-    ],
-  },
-  {
-    id: 7,
-    operation_type: 0,
-    address: "7000 Jacob Gateway, Durganport, WV 48044",
-    monthly_rent: 1000,
-    property_type: 0,
-    bedrooms: 3,
-    bathrooms: 2,
-    area: 180,
-    pets_allowed: true,
-    photo_url: [
-      "src/assets/images/background-example.jpg",
-      "src/assets/images/background-example.jpg",
-      "src/assets/images/background-example.jpg",
-      "src/assets/images/background-example.jpg",
-      "src/assets/images/background-example.jpg",
-    ],
-  },
-];
+    getMyFavorites()
+      .then((favorites) => {
+        setFavorites(favorites);
+      })
+      .catch((e) => console.log(e));
 
-const own_properties_mock = [
-  {
-    id: 9,
-    operation_type: 0,
-    address: "9000 Jacob Gateway, Durganport, WV 48044",
-    monthly_rent: 3000,
-    property_type: 0,
-    bedrooms: 4,
-    bathrooms: 2,
-    area: 180,
-    pets_allowed: false,
-    photo_url: [
-      "src/assets/images/background-example.jpg",
-      "src/assets/images/background-example.jpg",
-      "src/assets/images/background-example.jpg",
-      "src/assets/images/background-example.jpg",
-      "src/assets/images/background-example.jpg",
-    ],
-  },
-];
-
-export default function BestPropertiesSection({
-  best_properties,
-  favorites,
-  own_properties,
-}) {
-  // best_properties = best_mock;
-  favorites = favorites_mock;
-  own_properties = own_properties_mock;
+    getMyProperties()
+      .then((ownProperties) => {
+        setOwnProperties(ownProperties);
+      })
+      .catch((e) => console.log(e));
+  }, []);
 
   return (
     <StyledSection>
@@ -181,9 +47,11 @@ export default function BestPropertiesSection({
           <StyledP>Find an Apartment you Love</StyledP>
           <StyledH2>Homes for rent at the best prices</StyledH2>
           <PropertiesContainer>
-            {best_properties.map((property) => {
-              const isFavorite = true;
-              const isOwner = true;
+            {properties.slice(0, 3).map((property) => {
+              const isOwner = ownProperties.some((e) => e.id === property.id);
+              const isFavorite = favorites.properties?.some(
+                (e) => e.id === property.id
+              );
 
               return (
                 <PropertyCard
