c57dc331828fdff806476e3c45a6ab5b67d75063
Gabriela Cruz <gcruzti@gmail.com>
2023-07-26 13:05:12 -0300

    edit rent and sale pages

--- src/pages/EditSalePropertyPage/edit-sale-property-page.jsx
+++ src/pages/EditSalePropertyPage/edit-sale-property-page.jsx
@@ -1,3 +1,258 @@
-export default function EditSalePropertyPage() {
-  return <div>Edit Sale Property Page</div>;
+import GooglePlacesAutocomplete from "react-google-autocomplete";
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { HiMagnifyingGlass } from "react-icons/hi2";
+
+import Select from "../../components/Select";
+import Input from "../../components/Input";
+import Button from "../../components/Button";
+import HeaderLandlord from "../../components/HeaderLandlord";
+import Container from "../../layout/Container";
+import { StyledContainer, StyledTextArea } from "./styles";
+import InputWithIcon from "../../components/InputWithIcon";
+import { TbCoin } from "react-icons/tb";
+import Footer from "../../components/Footer";
+import { useAuth } from "../../context/auth-context";
+import { createProperty } from "../../services/property-service";
+
+function EditSalePropertyPage() {
+  const { user } = useAuth();
+
+  const [formData, setFormData] = useState({
+    address: "",
+    district: "",
+    state: "",
+    montly_rent: null,
+    maintenance: null,
+    property_type: null,
+    bedrooms: null,
+    bathrooms: null,
+    area: null,
+    pets_allowed: null,
+    about: "",
+    operation_type: "sale",
+    user_id: user?.id,
+  });
+
+  const [images, setImages] = useState([]);
+
+  function handleChange(event) {
+    const name = event.target.name;
+    const value =
+      event.target.type === "checkbox"
+        ? event.target.checked
+        : event.target.value;
+    const newValues = { ...formData, [name]: value };
+    setFormData(newValues);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const propertyData = new FormData();
+
+    for (const [key, value] of Object.entries(formData)) {
+      propertyData.append(key, value);
+    }
+
+    for (let i = 0; i < images.length; i++) {
+      propertyData.append("images[]", images[i]);
+    }
+
+    createProperty(propertyData)
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  function handleImageChange(e) {
+    const selectedImages = Array.from(e.target.files);
+    setImages([...images, ...selectedImages]);
+  }
+
+  function handleAddressChange(address) {
+    setFormData({
+      ...formData,
+      address: address.formatted_address,
+    });
+  }
+
+  return (
+    <>
+      <HeaderLandlord />
+      <main>
+        <Container size="xl">
+          <StyledContainer>
+            <h1 className="title">Edit a property listing</h1>
+            <p className="label">OPERATION TYPE</p>
+            <div>
+              <div className="links-container">
+                <NavLink
+                  to="/property/create/rent"
+                  className={({ isActive }) =>
+                    isActive ? "link-left active" : "link-left"
+                  }
+                >
+                  Rent
+                </NavLink>
+                <NavLink
+                  to="/property/create/sale"
+                  className={({ isActive }) =>
+                    isActive ? "link-right active" : "link-right"
+                  }
+                >
+                  Sale
+                </NavLink>
+              </div>
+            </div>
+            <form className="form" onSubmit={handleSubmit}>
+              <label className="label">ADDRESS</label>
+              <div className="container-autocomplete ">
+                <HiMagnifyingGlass size={"1.25rem"} />
+                <GooglePlacesAutocomplete
+                  apiKey={import.meta.env.VITE_GOOGLE_API_KEY}
+                  autocompletionRequest={{
+                    types: ["address"],
+                  }}
+                  selectProps={{
+                    name: "address",
+                    onChange: handleAddressChange,
+                    placeholder: "Start typing to autocomplete ",
+                  }}
+                />
+              </div>
+              <InputWithIcon
+                label={"DISTRICT"}
+                icon={<HiMagnifyingGlass size={"1.25rem"} />}
+                isFullWidth
+                name="district"
+                type="text"
+                placeholder="District"
+                onChange={handleChange}
+              />
+              <InputWithIcon
+                label={"STATE"}
+                icon={<HiMagnifyingGlass size={"1.25rem"} />}
+                isFullWidth
+                name="state"
+                type="text"
+                placeholder="STATE"
+                onChange={handleChange}
+              />
+              <InputWithIcon
+                label="PRICE"
+                icon={<TbCoin size={"1.25rem"} />}
+                type="number"
+                name="price"
+                placeholder="2000"
+                onChange={handleChange}
+              />
+              <Select
+                label={"PROPERTY TYPE"}
+                name="property_type"
+                options={[
+                  { label: "Apartment", value: "apartment" },
+                  { label: "House", value: "house" },
+                ]}
+                defaultValue={"Select..."}
+                onChange={handleChange}
+              />
+              <div className="form__property">
+                <Select
+                  label={"BEDROOMS"}
+                  name="bedrooms"
+                  options={[
+                    { label: "1", value: "1" },
+                    { label: "2", value: "2" },
+                    { label: "3", value: "3" },
+                    { label: "4", value: "4" },
+                    { label: "5", value: "5" },
+                    { label: "6", value: "6" },
+                    { label: "7", value: "7" },
+                    { label: "8", value: "8" },
+                    { label: "9", value: "9" },
+                    { label: "10", value: "10" },
+                  ]}
+                  defaultValue={"Select..."}
+                  onChange={handleChange}
+                />
+                <Select
+                  label={"BATHROOMS"}
+                  name="bathrooms"
+                  options={[
+                    { label: "1", value: "1" },
+                    { label: "2", value: "2" },
+                    { label: "3", value: "3" },
+                    { label: "4", value: "4" },
+                    { label: "5", value: "5" },
+                    { label: "6", value: "6" },
+                    { label: "7", value: "7" },
+                    { label: "8", value: "8" },
+                    { label: "9", value: "9" },
+                    { label: "10", value: "10" },
+                  ]}
+                  defaultValue={"Select..."}
+                  onChange={handleChange}
+                />
+                <Input
+                  label="AREA IN M2"
+                  name="area"
+                  type="number"
+                  placeholder="##"
+                  onChange={handleChange}
+                />
+              </div>
+              <label>
+                <p className="label">ABOUT THIS PROPERTY</p>
+                <StyledTextArea
+                  name="about"
+                  placeholder="My Apartment is great because..."
+                  onChange={handleChange}
+                />
+                <blockquote className="quote">
+                  Renters will read this first, so highlight any features or
+                  important information the apartment has.
+                </blockquote>
+              </label>
+              <div>
+                <h3 className="photos-title">Photos</h3>
+                <label>
+                  <p className="photos-instructions">
+                    Upload as many photos as you wish
+                  </p>
+                  <input
+                    type="file"
+                    name="photo_url"
+                    multiple
+                    onChange={handleImageChange}
+                  />
+                  <blockquote className="quote">
+                    Only images, max 5MB
+                  </blockquote>
+                </label>
+                <div className="images-container">
+                  {images.map((image, index) => (
+                    <img
+                      key={index}
+                      src={URL.createObjectURL(image)}
+                      className="images-container__image"
+                    />
+                  ))}
+                </div>
+              </div>
+              <Button type="primary" size="lg">
+                UPDATE PROPERTY LISTING
+              </Button>
+            </form>
+          </StyledContainer>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 }
+
+export default EditSalePropertyPage;
---
+++ src/pages/EditSalePropertyPage/styles.js
@@ -0,0 +1,161 @@
+import styled from "@emotion/styled";
+import { fonts } from "../../styles";
+
+export const StyledContainer = styled.div`
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  font-family: ${fonts.secondary};
+
+  .title {
+    font-size: 2.25rem;
+    line-height: 3rem;
+    letter-spacing: 0.01563rem;
+  }
+
+  .links-container {
+    display: flex;
+  }
+
+  .link-left {
+    border-top: 0.0625rem solid
+      ${(props) => props.theme.colors.background.darker};
+    border-bottom: 0.0625rem solid
+      ${(props) => props.theme.colors.background.darker};
+    border-left: 0.0625rem solid
+      ${(props) => props.theme.colors.background.darker};
+    padding: 0.5rem;
+    border-top-left-radius: 0.5rem;
+    border-bottom-left-radius: 0.5rem;
+  }
+
+  .link-right {
+    border: 0.0625rem solid ${(props) => props.theme.colors.background.darker};
+    padding: 0.5rem;
+    border-top-right-radius: 0.5rem;
+    border-bottom-right-radius: 0.5rem;
+  }
+
+  .active {
+    background-color: ${(props) => props.theme.colors.pink[400]};
+  }
+
+  .form {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    align-items: flex-start;
+  }
+
+  .label-container {
+    display: flex;
+    gap: 0.25rem;
+    font-size: 0.875rem;
+    margin-bottom: 0.25rem;
+  }
+
+  .label {
+    color: ${(props) => props.theme.colors.text.light};
+    font-size: 0.625rem;
+    font-family: ${fonts.secondary};
+    letter-spacing: 0.09375rem;
+    //word-wrap: break-word;
+  }
+
+  .form__type {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+  }
+
+  .form__property {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+  }
+
+  .form__pets {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+  }
+
+  .quote {
+    color: ${(props) => props.theme.colors.text.light};
+    font-family: Inter;
+    font-size: 0.75rem;
+    letter-spacing: 0.025rem;
+  }
+
+  .images-container {
+    background-color: ${(props) => props.theme.colors.background.lighter};
+    max-height: 8.5rem;
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+    padding: 0.5rem;
+  }
+
+  .images-container__image {
+    max-height: 7.5rem;
+    max-width: 7.5rem;
+    object-fit: cover;
+    aspect-ratio: 1/1;
+  }
+
+  .photos-title {
+    font-family: Montserrat;
+    font-size: 1.25rem;
+    font-weight: 500;
+    line-height: 1.75rem; /* 140% */
+    letter-spacing: 0.00938rem;
+  }
+
+  .photos-instructions {
+    font-size: 0.625rem;
+    letter-spacing: 0.09375rem;
+  }
+
+  .container-autocomplete {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+    border: 1px solid ${(props) => props.theme.colors.pink[400]};
+    border-radius: 0.5rem;
+    padding: 0.5rem;
+  }
+
+  .container-autocomplete input[type="text"] {
+    accent-color: ${(props) => props.theme.colors.pink[400]};
+    position: relative;
+
+    width: 100%;
+    padding: 0.25rem 0;
+    border: none;
+  }
+
+  .container-autocomplete input::placeholder {
+    color: ${(props) => props.theme.colors.text.light};
+    font-family: ${fonts.secondary};
+    letter-spacing: 0.03125rem;
+  }
+
+  .container-autocomplete input:focus {
+    outline: 0.1875rem solid ${(props) => props.theme.colors.info};
+  }
+`;
+
+export const StyledTextArea = styled.textarea`
+  padding: 0.5rem;
+  font-family: Inter;
+  font-size: 0.875rem;
+  line-height: 1.25rem; /* 142.857% */
+  letter-spacing: 0.01563rem;
+  border: 1px solid ${(props) => props.theme.colors.pink[400]};
+  border-radius: 0.5rem;
+
+  ::placeholder {
+    color: ${(props) => props.theme.colors.text.light};
+  }
+`;
